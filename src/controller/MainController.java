f5326ba564a133aa47a04fa9b5cb70bb782cf6ae
Ebubekir Tosun <tosunbekir91@gmail.com>
2025-04-04 22:43:43 +0200

    FXML

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,5 +1,30 @@
 package controller;
 
-public class MainController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainController {
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField studentNumberField;
+    @FXML
+    private TextField studentGradeField;
+    @FXML
+    private Label outputLabel;
+
+    private List<Integer> grades = new ArrayList<>();
+
+
+    @FXML
+    public void addGradeButton(ActionEvent actionEvent) {
+
+    }
 }
